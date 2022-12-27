e75ec8d2860b41023aff90876886de1fd8fa5579
Carsten Jahn <carsten.jahn@bayer.com>
2022-12-27 09:20:18 +0100

    DB schema, work in progress...

---
+++ database_schema/cellenium.sql
@@ -0,0 +1,230 @@
+create
+extension if not exists plpython3u;
+
+
+create table ontology
+(
+    ontid    int primary key,
+    name     text,
+    umls_sab text
+    --cid_range_lower int, -- unused, could be used to filter concept based on ontology, to speed things up
+    --cid_range_upper int
+);
+
+create table concept
+(
+    cid                  serial primary key,
+    ontid                int references ontology,
+    ont_code             text,
+    label                text,
+    label_tsvector       tsvector,
+    umls_disease         boolean,
+    umls_semantic_types  text[],
+    umls_semantic_groups text[]
+);
+create unique index concept_i1 on concept (ontid, ont_code);
+create index concept_i2 on concept (lower(label), ontid, cid);
+
+create table concept_synonym
+(
+    cid              int not null references concept,
+    synonym          text,
+    synonym_tsvector tsvector
+);
+create index concept_synonym_i1 on concept_synonym (cid);
+
+
+create table concept_hierarchy
+(
+    cid        int references concept,
+    parent_cid int references concept
+);
+-- alternative: store the full parent-path(s) for each cid using the ltree data type, see e.g.
+-- https://hoverbear.org/blog/postgresql-hierarchical-structures/
+create unique index concept_hierarchy_i1 on concept_hierarchy (cid, parent_cid);
+create unique index concept_hierarchy_i2 on concept_hierarchy (parent_cid, cid);
+
+
+CREATE TABLE study
+(
+    study_id                serial primary key,
+    study_name              text not null,
+    description             text,
+    cluster_color_map       jsonb,
+
+    --attributes              text[],
+
+    cluster_hulls           jsonb,
+    plot_coords             jsonb,
+    h5adfile_modified_date  timestamptz,
+    import_status           text,
+    import_status_updated   timestamptz,
+    attribute_value_freq    jsonb,
+    cell_count              int,
+--     qc_status               text,
+--     qc_result               jsonb,
+
+    -- for subsampling:
+    projection_cell_coords  jsonb,
+    projection_cell_indices jsonb
+);
+
+-- gene, region etc
+CREATE TABLE omics
+(
+    omics_id         serial primary key,
+    omics_type       text not null,
+
+    tax_id           int  not null,
+    display_symbol   text not null,
+    display_name     text,
+
+    -- if omics_type=='gene'
+    ensembl_gene_id  text,
+    entrez_gene_id   text,
+    hgnc_symbol      text,
+
+    -- references omics_element, used if omics_type=='transcription_factor' or 'region' or 'CITE-seq'
+    linked_genes     int[] not null default array[]:: int [],
+
+    -- if omics_type=='CITE-seq' (protein antibody tag)
+    antibody_symbol  text,
+    antibody_name    text,
+--     gene_symbols         text[],
+--     ensembl_gene_ids     text[],
+
+    -- if omics_type=='transcription_factor' (promoter)
+    jaspar_matrix_id text,
+
+    -- if omics_type=='region'
+    build            text,
+    region_chr       text,
+    region_start     int,
+    region_end       int
+);
+create unique index omics_element_uq_gene on omics_element (tax_id, ensembl_gene_id);
+create unique index omics_element_uq_antibody on omics_element (tax_id, antibody_symbol);
+create unique index omics_element_uq_promoter on omics_element (tax_id, jaspar_matrix_id);
+create unique index omics_element_uq_region on omics_element (tax_id, build, region_chr, region_start, region_end);
+
+CREATE TABLE omics_region_gene
+(
+    omics_id        int not null,
+    constraint fk_omics_element_region_index
+        FOREIGN KEY (omics_element_id)
+            REFERENCES omics_element (id) ON DELETE CASCADE,
+    gene            text,
+    ensembl_gene_id text,
+    evidence        text,
+    evidence_score  real,
+    evidence_source text
+);
+create unique index omics_region_uq on omics_region_gene (omics_id, gene, evidence, evidence_source);
+
+-- e.g. in annotation category 'cell ontology name'
+CREATE TABLE sample_annotation
+(
+    sample_annotation_id serial not null,
+    h5ad_column          text   not null,
+    display              text   not null
+);
+
+-- e.g. in annotation category value 'lymphocyte'
+CREATE TABLE sample_annotation_value
+(
+    sample_annotation_value_id serial not null,
+    sample_annotation_id       int    not null,
+    constraint fk_sample_annotation
+        FOREIGN KEY (sample_annotation_id)
+            REFERENCES sample_annotation (sample_annotation_id),
+
+    h5ad_value                 text   not null,
+    display                    text   not null,
+    color                      text
+);
+
+CREATE TABLE study_sample_annotation_ui
+(
+    study_id                           int     not null,
+    constraint fk_study_id
+        FOREIGN KEY (study_id)
+            REFERENCES study (study_id) ON DELETE CASCADE,
+
+    sample_annotation_id               int     not null,
+    constraint fk_sample_annotation
+        FOREIGN KEY (sample_annotation_id)
+            REFERENCES sample_annotation (sample_annotation_id),
+
+    is_primary                         boolean not null,
+    ordering                           int     not null,
+    differential_expression_calculated boolean not null
+);
+
+CREATE TABLE study_sample
+(
+    study_id            int     not null,
+    constraint fk_study_id
+        FOREIGN KEY (study_id)
+            REFERENCES study (study_id) ON DELETE CASCADE,
+
+    study_sample_id     int     not null,
+    h5ad_obs_index      int     not null,
+    display_subsampling boolean not null,
+);
+create unique index study_sample_i1 on study_sample (study_id, study_sample_id);
+
+
+CREATE TABLE study_omics
+(
+    study_id         int not null,
+    constraint fk_study_id
+        FOREIGN KEY (study_id)
+            REFERENCES study (study_id) ON DELETE CASCADE,
+
+    omics_element_id int not null,
+    constraint fk_omics_element_region_index
+        FOREIGN KEY (omics_element_id)
+            REFERENCES omics_element (id) ON DELETE CASCADE,
+
+    -- indexing the h5ad .uns['protein_X'] matrix in this study
+    h5ad_var_index   int not null,
+    -- TODO add another h5ad_col_index for second h5ad file (ATAC-seq)?
+
+    -- region as seen in the actual study data before 'fuzzy' region matching with bedtools (expect same build, chromosome)
+    region_start     int,
+    region_end       int
+);
+
+CREATE TABLE differential_expression
+(
+    study_id                   int not null,
+    constraint fk_study_id
+        FOREIGN KEY (study_id)
+            REFERENCES study (id) ON DELETE CASCADE,
+    omics_id                   int,
+    constraint fk_omics_element
+        FOREIGN KEY (omics_id)
+            REFERENCES omics_element (id),
+
+    sample_annotation_id       int not null,
+    constraint fk_sample_annotation
+        FOREIGN KEY (sample_annotation_id)
+            REFERENCES sample_annotation (sample_annotation_id),
+
+-- differential expression of this group (sample_annotation_value_id) vs. all other groups
+    sample_annotation_value_id int not null,
+    constraint fk_sample_annotation_value
+        FOREIGN KEY (sample_annotation_value_id)
+            REFERENCES sample_annotation_value (sample_annotation_value_id),
+
+    pvalue                     float,
+    pvalue_adj                 float,
+    score                      float,
+    log2_foldchange            float
+);
+create unique index differential_expression_i1 on differential_expression (study_id, sample_annotation_id, sample_annotation_value_id, omics_id);
+
+CREATE TABLE expression
+(
+
+)
