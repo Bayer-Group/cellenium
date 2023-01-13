0b8ca26da7f664cdade3a60c125758eed9e6169f
andreassteffen <andreas.steffen@gmail.com>
2023-01-13 11:30:30 +0100

    tweaks

--- client/src/components/UserGene/UserGene.tsx
+++ client/src/components/UserGene/UserGene.tsx
@@ -10,12 +10,12 @@ const useStyles = createStyles((theme) => ({
     active: {
         backgroundColor: theme.colors.blue[3]
 
-    },
-    main: {
+    }, main: {
         "&:hover": {
             backgroundColor: theme.colors.blue[1]
         }
     }
+
 }));
 
 interface Props {
@@ -53,24 +53,26 @@ const UserGene = ({gene, buttons = []}: Props) => {
 
     return (
         <Group align={'center'} position={'left'} spacing={2} style={{width: '100%'}}>
-            <Grid columns={12} style={{width: '100%'}}>
+            <Grid columns={12} style={{width: '100%'}} gutter={'md'}>
                 <Grid.Col span={1}>
                     <ActionIcon variant={'subtle'} size={'xs'}>
                         <CloseButton onClick={() => handleRemove(gene)} size={'xs'} iconSize={15}/>
                     </ActionIcon>
                 </Grid.Col>
-                <Grid.Col span={4}>
-                    <Text size={'xs'}>{gene.displaySymbol}</Text>
-                </Grid.Col>
-                <Grid.Col span={4}>
-
+                <Grid.Col span={1}>
                     <ActionIcon
                         className={cx(classes.main, {[classes.active]: selectedGenes.filter((g) => g.omicsId === gene.omicsId).length !== 0})}
-                        onClick={() => handleColorClick(gene)} variant="default" size={'xs'} mr={5}>
+                        onClick={() => handleColorClick(gene)} variant="subtle" size={'xs'} mr={5}>
                         <IconEye/>
                     </ActionIcon>
-
                 </Grid.Col>
+                <Grid.Col span={3}>
+                    <Text size={'xs'}>{gene.displaySymbol}</Text>
+                </Grid.Col>
+                <Grid.Col span={7}>
+                    <Text size={'xs'} lineClamp={1}>{gene.displayName}</Text>
+                </Grid.Col>
+
             </Grid>
         </Group>
     );
