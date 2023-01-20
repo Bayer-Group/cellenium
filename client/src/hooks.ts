0509eea7e687c305aa0436214ff3c60b03a53046
Carsten Jahn <carsten.jahn@bayer.com>
2023-01-20 08:38:28 +0100

    fix the selected gene display in projection plot

--- client/src/hooks.ts
+++ client/src/hooks.ts
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useEffect, useMemo} from 'react';
 import * as aq from 'arquero';
 import {useExpressionByOmicsIdsQuery} from "./generated/types";
 import {ExpressionTable} from "./model";
@@ -24,24 +24,26 @@ export function useExpressionValues(omicsIds: number[]) {
         },
         skip: omicsIds.length === 0
     });
-    if (data?.expressionByOmicsIdsList) {
-        let t = aq.from(data?.expressionByOmicsIdsList);
-        // records contain two arrays, sampleIds and values, which have the same length and corresponding values.
-        // unfold both arrays and put values side by side in new rows.
-        if (t.numRows() > 0) {
-            const sampleIds = t.unroll('studySampleIds').select({studySampleIds: 'studySampleId'});
-            t = t.unroll('values').select({values: 'value', omicsId: 'omicsId'});
-            t = t.assign(sampleIds).reify()
-            return {
-                table: ExpressionTable.definedTable(t),
-                loading
-            };
+    return useMemo( () => {
+        if (data?.expressionByOmicsIdsList) {
+            let t = aq.from(data?.expressionByOmicsIdsList);
+            // records contain two arrays, sampleIds and values, which have the same length and corresponding values.
+            // unfold both arrays and put values side by side in new rows.
+            if (t.numRows() > 0) {
+                const sampleIds = t.unroll('studySampleIds').select({studySampleIds: 'studySampleId'});
+                t = t.unroll('values').select({values: 'value', omicsId: 'omicsId'});
+                t = t.assign(sampleIds).reify()
+                return {
+                    table: ExpressionTable.definedTable(t),
+                    loading
+                };
+            }
         }
-    }
-    return {
-        table: undefined,
-        loading
-    };
+        return {
+            table: undefined,
+            loading
+        };
+    }, [data, loading]);
 }
 
 export function useSetStudyFromUrl() {
