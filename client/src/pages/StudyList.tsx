9c0ab67b6f0f5827704e49af03d105bea11c05cf
Carsten Jahn <carsten.jahn@bayer.com>
2023-01-20 09:34:07 +0100

    filter studies on the front page when user adds entities

--- client/src/pages/StudyList.tsx
+++ client/src/pages/StudyList.tsx
@@ -1,29 +1,48 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {NavBar, SearchBar, StudyCard} from "../components";
 import {Container, Grid, Space} from "@mantine/core";
 import {StudyInfoFragment, useStudiesQuery} from "../generated/types";
 import {OntologyItem} from "../model";
 import {generateOntologyTrees} from "./helper";
+import {flatten} from "lodash";
 
 
 const StudyList = () => {
-    const {data, error, loading} = useStudiesQuery()
-    const [ontologyTrees, setOntologyTrees] = useState<Map<string, OntologyItem>>();
-    useEffect(() => {
-        if (data)
-            setOntologyTrees(generateOntologyTrees(data.treeOntologiesList))
-    }, [data])
+    const {data, error, loading} = useStudiesQuery();
+    const allStudies = useMemo(() => data?.studyOverviewsList && data?.studyOverviewsList.map(study => ({
+        ...study,
+        allOntCodes: [
+            study.studyOntologyList.map(ont => ont.ontCodes),
+            study.studyOntologyList.map(ont => ont.parentIds)
+        ].flat(2).filter(ontCode => !!ontCode)
+    })), [data]);
+
+    const ontologyTrees: Map<string, OntologyItem> | undefined = useMemo(
+        () => data?.treeOntologiesList && generateOntologyTrees(data.treeOntologiesList),
+        [data]);
+    const [searchOntCodes, setSearchOntCodes] = useState<string[]>([]);
+    console.log('searchOntCodes', searchOntCodes)
+
+    const filteredStudies = useMemo(() => {
+        if (!allStudies) {
+            return undefined;
+        }
+        if (searchOntCodes.length === 0) {
+            return allStudies;
+        }
+        return allStudies.filter(study => searchOntCodes.find(searchOntCode => study.allOntCodes.indexOf(searchOntCode) > -1));
+    }, [data?.studyOverviewsList, searchOntCodes]);
+
     return (
         <Container fluid={true}>
             <NavBar/>
             <Space h="xl"/>
             <Container size={'xl'} style={{paddingBottom: '2rem'}}>
-                {ontologyTrees && <SearchBar ontologies={ontologyTrees}/>}
+                {ontologyTrees && <SearchBar ontologies={ontologyTrees} onSearchElementsUpdate={setSearchOntCodes}/>}
             </Container>
             <Container size={'xl'}>
                 <Grid>
-                    {data && data.studyOverviewsList && data.studyOverviewsList.map((study: StudyInfoFragment) => {
-
+                    {filteredStudies && filteredStudies.map((study: StudyInfoFragment) => {
                         return <Grid.Col span={12} key={study.studyId}><StudyCard study={study}/></Grid.Col>
                     })}
                 </Grid>
