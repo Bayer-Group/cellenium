7bd8c8015c1a6a448b95a1a159e27629866e841c
andreassteffen <andreas.steffen@gmail.com>
2023-02-27 11:20:15 +0100

    bit nicer ontology selection

--- client/src/components/OntologyBrowser/OntologyTree.tsx
+++ client/src/components/OntologyBrowser/OntologyTree.tsx
@@ -13,6 +13,7 @@ const OntologyTree = ({ontology, handleAddOntologyItem}: Props) => {
     return (
         <Group position={'center'} align={'center'} grow>
             <Stack spacing={0} justify={'center'} align={'flex-start'}>
+                <Text size={'xs'} color={'dimmed'}>Choose from:</Text>
                 {[ontology].map((item: OntologyItem) => <OntologyBranch handleAddOntologyItem={handleAddOntologyItem} key={item.id} item={item} level={0}></OntologyBranch>)}
             </Stack>
         </Group>
