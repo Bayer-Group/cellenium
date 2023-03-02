f44087953558a074a030559f97fa7e126f37b478
Carsten Jahn <carsten.jahn@bayer.com>
2023-03-02 16:30:41 +0100

    remove old data import code

--- data_import/study_preparation.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    import public_data.pancreas
-    import public_data.study_list
-
-    public_data.pancreas.create_study_files()
-    public_data.study_list.create_study_files()
