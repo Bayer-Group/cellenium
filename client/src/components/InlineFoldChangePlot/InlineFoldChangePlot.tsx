cbcf8d0aaa0bac6f38cd46d98d623df48dfaa7df
Carsten Jahn <carsten.jahn@bayer.com>
2023-10-16 15:49:03 +0200

    p values of 0 get set to 10e-100 to avoid infinity in the log10 scaled plot

--- client/src/components/InlineFoldChangePlot/InlineFoldChangePlot.tsx
+++ client/src/components/InlineFoldChangePlot/InlineFoldChangePlot.tsx
@@ -20,6 +20,10 @@ export function InlineFoldChangePlot({
     },
   });
   if (loading) return <Loader variant="dots" color="blue" />;
+  // p values of 0 get set to 10e-100 to avoid infinity in the log10 scaled plot
+  const pvalues = data?.differentialExpressionsList && data.differentialExpressionsList.map((e) => (e.pvalueAdj === 0 ? 10 ** -100 : e.pvalueAdj));
+  const minPvalueAdj = (pvalues && Math.min(...pvalues)) || 10 ** -100;
+
   return (
     <Plot
       config={{
@@ -30,8 +34,8 @@ export function InlineFoldChangePlot({
       data={[
         {
           x: data?.differentialExpressionsList.map((e) => e.log2Foldchange),
-          y: data?.differentialExpressionsList.map((e) => {
-            const ppval = -1 * Math.log10(e.pvalueAdj);
+          y: pvalues?.map((e) => {
+            const ppval = -1 * Math.log10(e);
             return ppval || 0;
           }),
           type: 'scatter',
@@ -41,7 +45,7 @@ export function InlineFoldChangePlot({
         },
         {
           x: [log2fc],
-          y: [-1 * Math.log10(pval)],
+          y: [-1 * Math.log10(pval || minPvalueAdj)],
           marker: {
             color: 'red',
             size: 5,
@@ -55,7 +59,6 @@ export function InlineFoldChangePlot({
         margin: { l: 26, r: 5, t: 5, b: 20 },
 
         xaxis: {
-          range: [0, 10],
           visible: true,
           fixedrange: true,
           showgrid: false,
